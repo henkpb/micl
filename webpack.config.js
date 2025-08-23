edd2f21b18e7568b32ea8d75f251054c3577222d
Henk <henk@hermana.no>
2025-08-23 15:52:21 +0200

    Create outputs for the MICL Showcase as well

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const glob    = require('glob');
 const webpack = require('webpack');
 const miniCss = require('mini-css-extract-plugin');
 const distDir = path.resolve(__dirname, 'dist');
+const docsDir = path.resolve(__dirname, 'docs');
 
 const scssFiles = glob.sync('./components/**/*.scss');
 const scssEntries = scssFiles.reduce((entries, filePath) => {
@@ -24,6 +25,42 @@ module.exports = [{
         path: distDir,
         filename: '[name].js',
         clean: true,
+        library: {
+            name: 'micl',
+            type: 'umd'
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use : [
+                miniCss.loader,
+                'css-loader',
+                'postcss-loader',
+                'sass-loader'
+            ]
+        }, {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    plugins: [
+        new miniCss({
+            filename: '[name].css'
+        })
+    ]
+}, {
+    mode: 'production',
+    entry: {
+        micl: ['./styles.scss', './micl.ts']
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    output: {
+        path: docsDir,
+        filename: '[name].js',
         library: {
             name: 'micl',
             type: 'umd'
