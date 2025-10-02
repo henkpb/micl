04e1e9744a551d1f1c83822709558441322563a3
Henk <henk@hermana.no>
2025-10-02 17:16:11 +0200

    The Select component now has its own TypeScript file

--- components/select/index.ts
+++ components/select/index.ts
@@ -19,15 +19,12 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-export const textfieldSelector = '.micl-textfield-outlined > input,.micl-textfield-filled > input';
-export const textareaSelector  = '.micl-textfield-outlined > textarea,.micl-textfield-filled > textarea';
+export const selectSelector = '.micl-textfield-filled > select,.micl-textfield-outlined > select';
 
 export default (() =>
 {
-    const counterSelector = '.micl-textfield__character-counter';
-
     return {
-        initialize: (element: HTMLInputElement | HTMLTextAreaElement): void =>
+        initialize: (element: HTMLSelectElement): void =>
         {
             if (element.dataset.miclinitialized) {
                 return;
@@ -38,22 +35,25 @@ export default (() =>
                 element.dataset.miclvalue = '1';
             }
 
-            if (!element.maxLength) {
-                return;
-            }
+            element.addEventListener('mousedown', event =>
+            {
+                const
+                    rect      = element.getBoundingClientRect(),
+                    roomAbove = rect.top,
+                    roomBelow = window.innerHeight - rect.bottom;
 
-            const counter = element.parentElement?.querySelector(counterSelector);
-            if (counter) {
-                counter.textContent = `${element.value.length}/${element.maxLength}`;
-            }
+                !element.matches(':open') && element.style.setProperty(
+                    '--md-sys-select-picker-origin',
+                    roomAbove > roomBelow ? 'left bottom' : 'left top'
+                );
+            });
         },
 
         input: (event: Event): void =>
         {
             if (
-                !(event.target as Element).matches(`${textfieldSelector},${textareaSelector}`)
-                || !((event.target instanceof HTMLInputElement)
-                    || (event.target instanceof HTMLTextAreaElement))
+                !(event.target as Element).matches(`${selectSelector}`)
+                || !(event.target instanceof HTMLSelectElement)
                 || !event.target.dataset.miclinitialized
                 || event.target.disabled
             ) {
@@ -65,15 +65,6 @@ export default (() =>
             }
             else {
                 delete event.target.dataset.miclvalue;
-            }
-
-            if (!event.target.maxLength) {
-                return;
-            }
-
-            const counter = event.target.parentElement?.querySelector(counterSelector);
-            if (counter) {
-                counter.textContent = `${event.target.value.length}/${event.target.maxLength}`;
             }
         }
     };
--- dist/components/textfield/index.d.ts
+++ dist/components/textfield/index.d.ts
@@ -1,8 +1,7 @@
 export declare const textfieldSelector = ".micl-textfield-outlined > input,.micl-textfield-filled > input";
-export declare const selectSelector = ".micl-textfield-outlined > select,.micl-textfield-filled > select";
 export declare const textareaSelector = ".micl-textfield-outlined > textarea,.micl-textfield-filled > textarea";
 declare const _default: {
-    initialize: (element: HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement) => void;
+    initialize: (element: HTMLInputElement | HTMLTextAreaElement) => void;
     input: (event: Event) => void;
 };
 export default _default;
