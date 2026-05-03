b487f4be848c020c79c0d4d8ab678793d1eacae6
Henk <henk@hermana.no>
2026-05-03 12:36:04 +0200

    Added tokens not (yet) found in the MD3 specification

--- components/list/index.ts
+++ components/list/index.ts
@@ -1,5 +1,5 @@
 //
-// Copyright © 2026 Hermana AS
+// Copyright © 2025 Hermana AS
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
