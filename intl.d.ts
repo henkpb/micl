17c60319ae31b47cc4be89f0f96a28d38ed0aced
Henk <henk@hermana.no>
2026-09-25 16:53:04 +0200

    Not needed anymore

--- intl.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace Intl {
-    interface Locale {
-        getWeekInfo(): {
-            firstDay: 1 | 2 | 3 | 4 | 5 | 6 | 7;
-            weekend: number[];
-            minimalDays: 1 | 2 | 3 | 4 | 5 | 6 | 7;
-        };
-    }
-}
